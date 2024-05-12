42adf152929beef10ae29612382905be384af27c
DanieCervantes <daicervantes2@sfsu.edu>
2024-05-12 15:44:49 -0700

    updated 05/12

--- card.cpp
+++ card.cpp
@@ -10,6 +10,20 @@ void Card::display() {
 }
 
 int Card::getValue() {
-    // Implement logic to return the value of the card in Blackjack
-    return 0;
+    // For numbered cards (2 through 10), return their face value
+    if (rank == "2" || rank == "3" || rank == "4" || rank == "5" || rank == "6" || rank == "7" || rank == "8" || rank == "9" || rank == "10") {
+        return std::stoi(rank);
+    }
+    // For face cards (Jack, Queen, King), return 10
+    else if (rank == "Jack" || rank == "Queen" || rank == "King") {
+        return 10;
+    }
+    // For Ace, return 11 if adding 11 doesn't exceed 21, otherwise return 1
+    else if (rank == "Ace") {
+        return 11; // Assume default value is 11
+    }
+    // Default case (shouldn't happen in a well-formed deck)
+    else {
+        return 0;
+    }
 }
