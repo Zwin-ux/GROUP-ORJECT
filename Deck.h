5a29e78775581319c8792111c167285f7365a7e3
Zwin-ux <64337759+Zwin-ux@users.noreply.github.com>
2024-05-12 00:01:03 -0700

    Update Deck.h
    as

--- Deck.h
+++ Deck.h
@@ -1,19 +1,21 @@
 #ifndef DECK_H
 #define DECK_H
 
-#include "card.h" // Assuming the correct filename is "Card.h"
-#include <list>
+#include "Card.h"
+#include <vector>
 
 class Deck {
 private:
-    std::list<Card*> cards;
+    std::vector<Card*> cards;
+    int nextCardIndex;
+
 public:
     Deck();
     ~Deck();
     void initialize();
     void shuffle();
     Card* dealCard();
-    int cardsLeft();
+    int cardsLeft() const;
 };
 
 #endif
