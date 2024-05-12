694062cf36e22da76c791ca5ca5d70f213f99751
DanieCervantes <daicervantes2@sfsu.edu>
2024-05-12 16:01:57 -0700

    updated

--- BlackJack.cpp
+++ BlackJack.cpp
@@ -21,18 +21,21 @@ void BlackJack::startGame() {
     // Shuffle the deck
     deck->shuffle();
 
-     // Create players and deal initial cards
+    // Create players and deal initial cards
     for (int i = 0; i < 4; ++i) { // Max number of players is set to 4
-        std::string playerName;
-        std::cout << "Enter name for Player " << i + 1 << " (or type 'done' to finish): ";
-        std::cin >> playerName;
-
-        if (playerName == "done") { // Check if the user wants to stop adding players
+        if (i >= numPlayers) {
+            std::cout << "Only 4 players can play. Adding more players will be ignored." << std::endl;
             break;
         }
 
+        std::string playerName;
+        std::cout << "Enter name for Player " << i + 1 << ": ";
+        std::cin >> playerName;
+
         players.push_back(new Player(playerName));
     }
+    dealInitialCards();
+
 
     // Player turns
     for (int i = 0; i < numPlayers; ++i) {
