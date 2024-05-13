ffe260e7dfd9623da95776e622cfbc42d07b8051
DanieCervantes <daicervantes2@sfsu.edu>
2024-05-12 21:05:36 -0700

    updated

--- Deck.cpp
+++ Deck.cpp
@@ -14,7 +14,15 @@ Deck::~Deck() {
 }
 
 void Deck::initialize() {
+ // Generate a standard deck of 52 cards
+    std::vector<std::string> suits = {"Hearts", "Diamonds", "Clubs", "Spades"};
+    std::vector<std::string> ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
 
+    for (const auto& suit : suits) {
+        for (const auto& rank : ranks) {
+            cards.push_back(new Card(suit, rank));
+        }
+    }
 }
 
 void Deck::shuffle() {
